99215d7e99518bb906333d6610db81adbfe5db03
Sagar0201 <sagarkakade0201@gmail.com>
2025-05-29 11:27:14 +0530

    host

--- heartbeat/settings.py
+++ heartbeat/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t2&-#5=#cxcqw3#tqiqs$=da%dao!0r&=4b2b5l$_#iep$#8sn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['heartbeat.onrender.com', 'localhost','127.0.0.1','*']
 
 
 # Application definition
